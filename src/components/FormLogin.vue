fa9a550c9264ff55eeb6c9b25c303527b12116a6
GabrielTura <gabrieltura319@hotmail.com>
2020-02-12 18:32:21 -0300

    Making FormLogin - Mobile Layout

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -21,9 +21,96 @@
         </section>
         <router-link to="/dashboard"><b-button class="btn-login">Fazer Login</b-button></router-link>
       </div>
+      <div class="formLoginMobile">
+        <img class="logo-mobile" src="../assets/logo-dash-small.png"/>
+        <br>
+        <span>Acesse Sua Conta</span>
+        <section class="fields-custom">
+            <b-input type="email" placeholder="E-mail"></b-input>
+            <b-input type="password" placeholder="Senha">
+            </b-input>
+        </section>
+        <div class="field">
+          <a class="label-end">Esqueceu sua senha?</a>
+        </div>
+        <router-link to="/dashboard"><b-button class="btn-login">Fazer Login</b-button></router-link>
+      </div>
     </div>
 </template>
 <style>
+@media only screen and (max-width: 600px) {
+  .tt{
+    display: none;
+  }
+  .logo-mobile{
+    margin-top: -50px;
+    margin-bottom: 80px;
+  }
+  .formLogin {
+    display: none;
+  }
+  .footer-home{
+    display: none;
+  }
+  .fields-custom{
+    line-height: 45px;
+  }
+  .label-end{
+    margin-left: 55px !important;
+    color: #fff !important;
+    text-decoration-line: underline;
+    font-weight: 200;
+  }
+  .label-end:hover{
+    color: #fff !important;
+  }
+  .btn-login{
+    background-color: #D7311C !important;
+    width: 210px !important;
+  }
+  .btn-login:hover{
+    background-color: red !important;
+  }
+  ::placeholder {
+    opacity: 1;
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+  .gradient-color-overlay {
+    background: linear-gradient(226deg, #D9000FCC, #EA5703CC, #EBAE1DCC) !important;
+    opacity: 1;
+  }
+  .background{
+    background-size: cover !important;
+    background-position-x: -400px !important;
+  }
+  .formLoginMobile {
+    background-color:transparent;
+    width: 330px;
+    height: 450px;
+    position: absolute;
+    color: #fff;
+    font-weight: bold;
+    font-size: 17px;
+    padding-left: 60px;
+    padding-right: 60px;
+  }
+}
+@media only screen and (min-width: 600px) {
+  .formLogin{
+    display: block;
+  }
+  .tt{
+    display: none;
+  }
+  .btn-login{
+    background-color: #EBAE1D !important;
+  }
+  .formLoginMobile{
+    display: none;
+  }
+} 
+
 .label {
   color: #A5220C !important;
 }
@@ -32,12 +119,12 @@
   font-size: .9rem;
 }
 .label-end {
-  color: #979797 !important;
+  color: #979797;
   font-size: .9rem;
   margin-left: 157px;
 }
 .label-end:hover {
-  color: #707070 !important;
+  color: #707070;
 }
 .formLogin {
   background-color:#FFF;
@@ -56,7 +143,7 @@
 }
 .btn-login {
   margin-top: 25px;
-  background-color: #EBAE1D !important;
+  background-color: #EBAE1D;
   color: #FFF !important;
   width: 250px;
 }
@@ -64,7 +151,7 @@
   width: 100%;
   height: 100%;
   background: linear-gradient(45deg, rgb(235, 174, 29), rgb(234, 87, 3), rgb(217, 0, 15));
-  opacity: .7;
+  opacity: 0.7;
 }
 .background {
   background: #000 url("../assets/background-home.png");
@@ -74,5 +161,6 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  background-size: cover !important;
 }
 </style>
